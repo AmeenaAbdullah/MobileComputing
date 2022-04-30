bac0a1eb9e9213e74cd2804b3b4bb49cd2a6de98
Ameena <meenamughal886@gmail.com>
2022-04-30 13:25:06 -0700

    calculator design

--- Calculator/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Calculator/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,18 +3,39 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
 public class MainActivity extends AppCompatActivity {
-    Button b1,b2,b3,b4,b5,b6,b7,b8,b9,b0;
+    Button one,two,three,four,five,six,seven,eight,nine,zero,clear,add,sub,div,mul,equal;
+    TextView display;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        b1=findViewById(R.id.)
+        display=findViewById(R.id.display);
+        one=findViewById(R.id.one);
+        two=findViewById(R.id.two);
+        three=findViewById(R.id.three);
+        four=findViewById(R.id.four);
+        five=findViewById(R.id.five);
+        six=findViewById(R.id.six);
+        seven=findViewById(R.id.seven);
+        eight=findViewById(R.id.eight);
+        nine=findViewById(R.id.nine);
+        zero=findViewById(R.id.zero);
 
-        public void zerobtn(){
+        clear=findViewById(R.id.clear);
+        equal=findViewById(R.id.equal);
+
+        add=findViewById(R.id.add);
+        sub=findViewById(R.id.sub);
+        mul=findViewById(R.id.mul);
+        div=findViewById(R.id.div);
 
 
-        }
     }
 }
