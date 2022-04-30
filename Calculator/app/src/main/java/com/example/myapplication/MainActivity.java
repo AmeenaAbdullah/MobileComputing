c9e4d589b9e520699fbefd16bfd30c408226a6d9
Ameena <meenamughal886@gmail.com>
2022-04-30 13:02:50 -0700

    design added

--- Calculator/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Calculator/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,18 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
-
+    Button b1,b2,b3,b4,b5,b6,b7,b8,b9,b0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        b1=findViewById(R.id.)
+
+        public void zerobtn(){
+
+
+        }
     }
 }
