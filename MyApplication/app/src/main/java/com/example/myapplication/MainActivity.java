8227c6f5451edc4a5fdacf81b43450d1c23def34
Ameena <meenamughal886@gmail.com>
2022-04-30 11:22:03 -0700

    counter complete

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,10 +3,15 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
 public class MainActivity extends AppCompatActivity {
 
     Button reset,increase,decrease;
+    int counter=0;
+    TextView txt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,6 +19,28 @@ public class MainActivity extends AppCompatActivity {
         increase=findViewById(R.id.button2);
         decrease=findViewById(R.id.button3);
         reset=findViewById(R.id.button);
+        txt=findViewById(R.id.textView);
+        increase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                counter++;
+                txt.setText(Integer.toString(counter));
+            }
+        });
 
+        decrease.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                counter--;
+                txt.setText(Integer.toString(counter));
+            }
+        });
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                counter=0;
+                txt.setText(Integer.toString(counter));
+            }
+        });
     }
 }
