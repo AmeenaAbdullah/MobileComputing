0f7b8f2c2ee9c10432c7c0cf226dc1270ad3f981
Ameena <meenamughal886@gmail.com>
2022-05-18 11:44:50 -0700

    implicit intent

--- Intent/app/src/main/java/com/example/aftermids/MainActivity.java
+++ Intent/app/src/main/java/com/example/aftermids/MainActivity.java
@@ -1,14 +1,36 @@
 package com.example.aftermids;
-
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.net.Uri;
 import android.os.Bundle;
-
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button sendAnother = findViewById(R.id.button);
+        sendAnother.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent send = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(send);
+            }
+        });
+
+
+        Button Dialer=findViewById(R.id.button2);
+        Dialer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("tel:+923001234567");
+                Intent intent = new Intent(Intent.ACTION_DIAL, uri);
+                startActivity(intent);
+            }
+        });
+
     }
+
 }
