2a50e484e0a130be7fbbbbc873f563b4cd8dd757
Ameena <meenamughal886@gmail.com>
2022-05-18 06:41:13 -0700

    updated

--- Intent/app/src/main/java/com/example/aftermids/MainActivity.java
+++ Intent/app/src/main/java/com/example/aftermids/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.myapplication;
+package com.example.aftermids;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,6 +10,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //TODO: have to implemetn
     }
 }
